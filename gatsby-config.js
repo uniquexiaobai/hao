7ea7bfb6b438d1de355191ac0daee9504178c0dc
uniquexiaobai <unique_xiaobai@qq.com>
2019-08-03 22:33:25 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CheatSheet`,
+    description: `Develop CheatSheet`,
+    author: `@lokibai`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `CheatSheet`,
+        short_name: `CheatSheet`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,96 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const list = [
+    {
+        name: 'Vim',
+        url: 'https://vim.rtorr.com/lang/zh_cn',
+    },
+    {
+        name: 'Sublime',
+        url: 'http://sublime.emptystack.net/',
+    },
+    {
+        name: 'Emoji',
+        url: 'https://gitmoji.carloscuesta.me/',
+    },
+    {
+        name: 'Flexbox',
+        url: 'https://darekkay.com/dev/flexbox-cheatsheet.html',
+    },
+    {
+        name: 'CSS Triggers',
+        url: 'https://csstriggers.com/',
+    },
+    {
+        name: 'Async',
+        url:
+            'https://raw.githubusercontent.com/frontarm/async-javascript-cheatsheet/master/async-cheatsheet.png',
+    },
+    {
+        name: 'React',
+        url: 'http://www.developer-cheatsheets.com/react',
+    },
+    {
+        name: 'React Lifecycle',
+        url: 'http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/',
+    },
+    {
+        name: 'React Router',
+        url: 'http://www.developer-cheatsheets.com/react-router',
+    },
+    {
+        name: 'Redux',
+        url: 'http://www.developer-cheatsheets.com/redux',
+    },
+    {
+        name: 'Vue',
+        url: 'https://marozed.ma/vue-cheatsheet/',
+    },
+    {
+        name: 'Vuex',
+        url: 'https://vuejs-tips.github.io/vuex-cheatsheet/',
+    },
+    {
+        name: 'Jest',
+        url: 'https://devhints.io/jest',
+    },
+    {
+        name: 'Create App',
+        url: 'https://createapp.dev/',
+    },
+    {
+        name: 'Nginx Config',
+        url: 'https://nginxconfig.io/',
+    },
+    {
+        name: 'Big O',
+        url: 'https://www.bigocheatsheet.com/',
+    },
+    {
+        name: 'Python3',
+        url: 'https://www.pythonsheets.com/index.html',
+    },
+]
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Home" />
+
+        <div>
+            {list.map(item => (
+                <ul key={item.name} style={{ marginBottom: 0 }}>
+                    <li style={{ marginBottom: '.5rem' }}>
+                        <a href={item.url} style={{ textDecoration: 'none' }}>
+                            {item.name}
+                        </a>
+                    </li>
+                </ul>
+            ))}
+        </div>
+    </Layout>
 )
 
 export default IndexPage
