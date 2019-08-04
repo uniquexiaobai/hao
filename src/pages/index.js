d55e4970107fcbed9eece29c3de8e0e897eb522d
uniquexiaobai <unique_xiaobai@qq.com>
2019-08-04 17:35:38 +0800

    update style and list

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,80 +2,151 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import '../styles/index.css'
 
-const list = [
+const cards = [
     {
-        name: 'Carbon',
-        url: 'https://carbon.now.sh',
+        title: 'Image',
+        list: [
+            {
+                name: 'Carbon',
+                url: 'https://carbon.now.sh',
+            },
+            {
+                name: 'Squoosh',
+                url: 'https://squoosh.app/',
+            },
+            {
+                name: '智图',
+                url: 'https://zhitu.isux.us/',
+            },
+        ]
     },
     {
-        name: 'Vim',
-        url: 'https://vim.rtorr.com/lang/zh_cn',
+        title: 'CSS',
+        list: [
+            {
+                name: 'Gradient',
+                url: 'https://cssgradient.io/',
+            },
+            {
+                name: 'RGB to',
+                url: 'https://rgb.to/',
+            },
+            {
+                name: 'Flexbox',
+                url: 'https://darekkay.com/dev/flexbox-cheatsheet.html',
+            },
+            {
+                name: 'Grid',
+                url: 'http://grid.malven.co/',
+            },
+            {
+                name: 'Grid Generator',
+                url: 'https://cssgrid-generator.netlify.com/',
+            },
+            {
+                name: 'CSS Triggers',
+                url: 'https://csstriggers.com/',
+            },
+        ]
     },
     {
-        name: 'Sublime',
-        url: 'http://sublime.emptystack.net/',
+        title: 'JavaScript',
+        list: [
+            {
+                name: 'Regexly',
+                url: 'https://regexly.chipto.io/',
+            },
+            {
+                name: 'JS Features',
+                url: 'https://jsfeatures.in/',
+            },
+            {
+                name: 'Async',
+                url:
+                    'https://raw.githubusercontent.com/frontarm/async-javascript-cheatsheet/master/async-cheatsheet.png',
+            },
+            {
+                name: 'AST Explorer',
+                url: 'https://astexplorer.net/',
+            },
+        ]
     },
     {
-        name: 'Emoji',
-        url: 'https://gitmoji.carloscuesta.me/',
+        title: 'Lib',
+        list: [
+            {
+                name: 'React',
+                url: 'http://www.developer-cheatsheets.com/react',
+            },
+            {
+                name: 'React Lifecycle',
+                url: 'http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/',
+            },
+            {
+                name: 'React Router',
+                url: 'http://www.developer-cheatsheets.com/react-router',
+            },
+            {
+                name: 'Redux',
+                url: 'http://www.developer-cheatsheets.com/redux',
+            },
+            {
+                name: 'Vue',
+                url: 'https://marozed.ma/vue-cheatsheet/',
+            },
+            {
+                name: 'Vuex',
+                url: 'https://vuejs-tips.github.io/vuex-cheatsheet/',
+            },
+            {
+                name: 'Jest',
+                url: 'https://devhints.io/jest',
+            },
+        ]
     },
     {
-        name: 'Flexbox',
-        url: 'https://darekkay.com/dev/flexbox-cheatsheet.html',
+        title: 'Tool',
+        list: [
+            {
+                name: 'Vim',
+                url: 'https://vim.rtorr.com/lang/zh_cn',
+            },
+            {
+                name: 'Sublime',
+                url: 'http://sublime.emptystack.net/',
+            },
+            {
+                name: 'Create App',
+                url: 'https://createapp.dev/',
+            },
+            {
+                name: 'Nginx Config',
+                url: 'https://nginxconfig.io/',
+            },
+            {
+                name: 'Manifest Generator',
+                url: 'https://tomitm.github.io/appmanifest/',
+            },
+        ]
     },
     {
-        name: 'CSS Triggers',
-        url: 'https://csstriggers.com/',
-    },
-    {
-        name: 'Async',
-        url:
-            'https://raw.githubusercontent.com/frontarm/async-javascript-cheatsheet/master/async-cheatsheet.png',
-    },
-    {
-        name: 'React',
-        url: 'http://www.developer-cheatsheets.com/react',
-    },
-    {
-        name: 'React Lifecycle',
-        url: 'http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/',
-    },
-    {
-        name: 'React Router',
-        url: 'http://www.developer-cheatsheets.com/react-router',
-    },
-    {
-        name: 'Redux',
-        url: 'http://www.developer-cheatsheets.com/redux',
-    },
-    {
-        name: 'Vue',
-        url: 'https://marozed.ma/vue-cheatsheet/',
-    },
-    {
-        name: 'Vuex',
-        url: 'https://vuejs-tips.github.io/vuex-cheatsheet/',
-    },
-    {
-        name: 'Jest',
-        url: 'https://devhints.io/jest',
-    },
-    {
-        name: 'Create App',
-        url: 'https://createapp.dev/',
-    },
-    {
-        name: 'Nginx Config',
-        url: 'https://nginxconfig.io/',
-    },
-    {
-        name: 'Big O',
-        url: 'https://www.bigocheatsheet.com/',
-    },
-    {
-        name: 'Python3',
-        url: 'https://www.pythonsheets.com/index.html',
+        title: 'Other',
+        list: [
+            {
+                name: 'Emoji',
+                url: 'https://gitmoji.carloscuesta.me/',
+            },
+            {
+                name: 'Big O',
+                url: 'https://www.bigocheatsheet.com/',
+            },
+            {
+                name: 'Python3',
+                url: 'https://www.pythonsheets.com/index.html',
+            },
+        ]
     },
 ]
 
@@ -84,14 +155,17 @@ const IndexPage = () => (
         <SEO title="Home" />
 
         <div>
-            {list.map(item => (
-                <ul key={item.name} style={{ marginBottom: 0 }}>
-                    <li style={{ marginBottom: '.5rem' }}>
-                        <a href={item.url} style={{ textDecoration: 'none' }}>
-                            {item.name}
-                        </a>
-                    </li>
-                </ul>
+            {cards.map(card => (
+                <div key={card.title} className="card">
+                    <h3>{card.title}</h3>
+                    <div className="list">
+                        {card.list.map((item) => (
+                            <a href={item.url} className="item">
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                </div>
             ))}
         </div>
     </Layout>
