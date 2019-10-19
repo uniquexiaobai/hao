7eccd8c30e96adb0beb30797288466a7c8973b68
uniquexiaobai <uniquexiaobai@gmail.com>
2019-10-19 22:57:11 +0800

    format code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,32 @@
 module.exports = {
-    siteMetadata: {
-        title: `CheatSheet`,
-        description: `Develop CheatSheet`,
-        author: `@lokibai`,
-    },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `CheatSheet`,
-                short_name: `CheatSheet`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/icon.png`, // This path is relative to the root of the site.
-            },
-        },
-        `gatsby-plugin-offline`,
-    ],
+	siteMetadata: {
+		title: `CheatSheet`,
+		description: `Develop CheatSheet`,
+		author: `@lokibai`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `CheatSheet`,
+				short_name: `CheatSheet`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		`gatsby-plugin-offline`,
+	],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,43 +5,43 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from './header'
-import '../styles/layout.css'
+import Header from './header';
+import '../styles/layout.css';
 
 const Layout = ({ children }) => {
-    const data = useStaticQuery(graphql`
-        query SiteTitleQuery {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `)
+	const data = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`);
 
-    return (
-        <>
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <div
-                style={{
-                    margin: `0 auto`,
-                    maxWidth: 960,
-                    padding: `0px 1.0875rem 1.45rem`,
-                    paddingTop: 0,
-                }}
-            >
-                <main>{children}</main>
-            </div>
-        </>
-    )
-}
+	return (
+		<>
+			<Header siteTitle={data.site.siteMetadata.title} />
+			<div
+				style={{
+					margin: `0 auto`,
+					maxWidth: 960,
+					padding: `0px 1.0875rem 1.45rem`,
+					paddingTop: 0,
+				}}
+			>
+				<main>{children}</main>
+			</div>
+		</>
+	);
+};
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
-}
+	children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
