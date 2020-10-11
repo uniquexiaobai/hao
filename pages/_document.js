3ed09911868d269c7fba3580537ac2a6f1a23e44
uniquexiaobai <uniquexiaobai@gmail.com>
2020-10-11 17:18:20 +0800

    add ga

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                  dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+
+                gtag('config', '${GA_TRACKING_ID}');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
